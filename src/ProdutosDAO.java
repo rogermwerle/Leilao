2390ab07befe81a235507dcc9b50d0a0e1f6212c
Roger Werle <roger.m.werle@gmail.com>
2024-08-06 21:37:58 -0300

    salvar produtos
    novo metodo criado para adicionar novos itens ao banco de dados.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,37 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
-    Connection conn;
+
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        try {
+            Connection conn = conectaDAO.conectar();
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES  (?,?,?)";
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            conectaDAO.encerrar(conn);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro inserindo : " + e.getMessage());
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
